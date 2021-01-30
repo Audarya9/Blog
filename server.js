5c7dd209d79eb1c0963a9cc769378202fdef3295
Oddiee <audaryarathod3@gmail.com>
2021-01-30 07:10:11 -0800

    edit and delete route created

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Article = require('./models/article');
+const methodOverride = require('method-override');
 const articleRoute = require('./routes/article');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/blog',{ useNewUrlParser: true , useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/blog',{ useNewUrlParser: true , useUnifiedTopology: true , useCreateIndex: true });
 
 app.set('view engine','ejs');
 
 app.use(express.urlencoded({ extended: false}));
+app.use(methodOverride('_method'));
 
 
 
 
 app.get('/', async (req,res)=>{
 
-    const article = await Article.find();
+    const article = await Article.find().sort({date: 'desc'});
     res.render('articles/index',{article : article});
 });
 
